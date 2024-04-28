7c3960791bd09338b36385e2534f4f8746e9de65
Thomash100 <hofmann@systemmedia.de>
2024-04-28 18:21:42 +0200

    Projektdateien hinzufügen.

---
+++ Projektverwaltung/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Projektverwaltung
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Geänderter Ereignishandler für 'Neu'
+        private void Neu_Click(object sender, RoutedEventArgs e)
+        {
+            // Öffnet ein neues Fenster anstelle des Speichern-Dialogs
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+
+        // Vorhandene Ereignisbehandlungsmethoden
+        // ...
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            btn_hello.Content = "Hallo";
+        }
+    }
+}
---
+++ Projektverwaltung/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projektverwaltung
+{
+    /// <summary>
+    /// Interaktionslogik für Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Weiter_Click(object sender, RoutedEventArgs e)
+        {
+            // Öffne das nächste Fenster
+            Window2 nextWindow = new Window2();
+            nextWindow.Show();
+
+            // Schließe dieses Fenster (optional)
+            this.Close();
+        }
+
+        private void Abbrechen_Click(object sender, RoutedEventArgs e)
+        {
+            // Schließe dieses Fenster
+            this.Close();
+        }
+    }
+}
